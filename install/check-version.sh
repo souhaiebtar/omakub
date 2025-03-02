3d7d76d07d1986852b8b0a836dadd9868d891b2c
Ryan Nowakowski <tubaman@fattuba.com>
2025-03-01 22:56:12 -0600

    changed to work with Debian 13(trixie)

--- install/check-version.sh
+++ install/check-version.sh
@@ -8,21 +8,11 @@ fi
 
 . /etc/os-release
 
-# Check if running on Ubuntu 24.04 or higher
-if [ "$ID" != "ubuntu" ] || [ $(echo "$VERSION_ID >= 24.04" | bc) != 1 ]; then
-  echo "$(tput setaf 1)Error: OS requirement not met"
-  echo "You are currently running: $ID $VERSION_ID"
-  echo "OS required: Ubuntu 24.04 or higher"
-  echo "Installation stopped."
-  exit 1
-fi
-
-# Check if running on x86
-ARCH=$(uname -m)
-if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "i686" ]; then
-  echo "$(tput setaf 1)Error: Unsupported architecture detected"
-  echo "Current architecture: $ARCH"
-  echo "This installation is only supported on x86 architectures (x86_64 or i686)."
-  echo "Installation stopped."
-  exit 1
+# Check if running on Debian 13 or higher
+if [ "$ID" != "debian" ] || ( [ $(echo "$VERSION_ID >= 13" | bc) != 1 ] || [ "$VERSION_CODENAME" != "trixie" ] ); then
+    echo "$(tput setaf 1)Error: OS requirement not met"
+    echo "You are currently running: $ID $VERSION_ID"
+    echo "OS required: Debian 13 or higher"
+    echo "Installation stopped."
+    exit 1
 fi
