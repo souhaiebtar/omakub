8764e199224fdb0e2d553249f2de635e48869a8d
tsouhaieb <tarhounisouhaieb@gmail.com>
2025-08-26 08:55:54 +0100

    WIP

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -3,7 +3,8 @@
 sudo apt install -y fzf ripgrep bat eza zoxide plocate apache2-utils fd-find 
 
 cd /tmp
+wget -qO tldr-hs.deb "http://archive.ubuntu.com/ubuntu/pool/universe/h/haskell-tldr/tldr-hs_0.9.2-6build1_amd64.deb"
 wget -qO tldr.deb "http://archive.ubuntu.com/ubuntu/pool/universe/h/haskell-tldr/tldr_0.9.2-6build1_all.deb"
-sudo apt --allow-downgrades install -y ./tldr.deb
-rm tldr.deb
+sudo apt --allow-downgrades install -y ./tldr-hs.deb ./tldr.deb
+rm tldr-hs.deb tldr.deb
 cd -
